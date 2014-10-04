94103ab6d2ec32f7a8899fe820abcbc408ad6000
hydralol <deathsama@hotmail.fr>
2014-10-04 23:06:49 +0100

    New update And FIx

---
+++ HShieldME/GX Auto Shield/Program.cs
@@ -0,0 +1,16 @@
+﻿using HShieldME;
+using System;
+
+namespace HShieldME
+{
+
+    class Program
+    {
+
+        private static void Main(String[] args)
+        {
+
+            new HShieldME();
+        }
+    }
+}
---
+++ HydraZilean/README.md
@@ -0,0 +1,5 @@
+LeagueSharp - Champion Assembly - Zilean
+-----------
+
+HydraZilean is an assembly for League Sharp that assists in playing the champion Zilean in League of Legends.
+
